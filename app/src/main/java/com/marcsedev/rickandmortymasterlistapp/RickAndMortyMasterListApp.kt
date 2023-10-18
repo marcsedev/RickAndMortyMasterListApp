c97d927f7641795e5d351e20ec9673221d6741e6
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-10-19 01:17:13 +0200

    init Dagger Hilt and add aplication file

---
+++ app/src/main/java/com/marcsedev/rickandmortymasterlistapp/RickAndMortyMasterListApp.kt
@@ -0,0 +1,8 @@
+package com.marcsedev.rickandmortymasterlistapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickAndMortyMasterListApp: Application() {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "RickAndMortyMasterListApp"
 include(":app")
-include(":app:common")
