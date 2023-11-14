6e9dfe5bc02a19df54745fffd596acc5e25f7d7f
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-11-14 14:20:11 +0100

    Add character Entity

---
+++ app/src/main/java/com/marcsedev/rickandmortymasterlistapp/data/database/entities/CharacterEntity.kt
@@ -0,0 +1,13 @@
+package com.marcsedev.rickandmortymasterlistapp.data.database.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "characters_table")
+data class CharacterEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val id: Long = 0, // Puedes cambiar el tipo de ID según tus necesidades
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "imageUrl") val imageUrl: String // Aquí puedes almacenar la URL de la imagen del personaje
+)
